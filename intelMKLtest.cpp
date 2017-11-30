988427ae80cbbc17aff97bc277cf00058804944c
donmegamuffin <donmegamuffin@users.noreply.github.com>
2017-11-30 12:34:12 +0000

    Stuff from the toy model

---
+++ intelMKLtest.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "intelMKLtest.h"
+
+
+intelMKLtest::intelMKLtest()
+{
+}
+
+
+intelMKLtest::~intelMKLtest()
+{
+}
---
+++ intelMKLtest.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "mkl.h"
+
+
+
+class intelMKLtest
+{
+public:
+	intelMKLtest();
+	~intelMKLtest();
+};
+
+
+
